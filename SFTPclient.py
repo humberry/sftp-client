f9934e0bb7a1a94498b208cb0840c82d4e5c7d0c
humberry <humberry@users.noreply.github.com>
2014-08-17 22:16:28 +0200

    Update SFTPclient.py

--- SFTPclient.py
+++ SFTPclient.py
@@ -10,47 +10,44 @@ class SFTPclient(ui.View):
     self.transport = None
     self.root = os.path.expanduser('~')
     self.path = os.getcwd()
-    pos = self.path.rfind('/')
-    self.cpath = self.path[pos:]
-    self.view = ui.load_view('SFTPclient')
-    bt1 = self.view['button1']
-    bt1.action = self.bt_connect
-    bt2 = self.view['button2']
-    bt2.action = self.bt_upload
-    bt3 = self.view['button3']
-    bt3.action = self.bt_download
-    self.tv1 = self.view['tableview1']
-    self.tv2 = self.view['tableview2']
-    self.txtv = self.view['textview1']
-    self.remotePath = '/home/' + self.view['textfield2'].text #user
+    self.view = ui.load_view('SFTPclient03')
+    self.view['bt_connect'].action = self.bt_connect
+    self.view['bt_upload'].action = self.bt_upload
+    self.view['bt_download'].action = self.bt_download
+    root_len = len(self.root)
+    self.view['lb_local'].text = self.path[root_len:]
+    self.tv_local = self.view['tv_local']
+    self.tv_remote = self.view['tv_remote']
+    self.tv_info = self.view['tv_info']
+    self.remotePath = '/home/' + self.view['tf_user'].text
     all = self.get_dir()
-    self.lst1 = ui.ListDataSource(all)
-    self.tv1.data_source = self.lst1
-    self.tv1.delegate = self.lst1
-    self.tv1.editing = False
-    self.lst1.font = ('Courier',14)
-    self.lst1.action = self.table1_tapped
-    self.lst1.delete_enabled = False 
-    self.lst2 = ui.ListDataSource([''])
-    self.tv2.data_source = self.lst2
-    self.tv2.delegate = self.lst2
-    self.tv2.editing = False
-    self.lst2.font = ('Courier',14)
-    self.lst2.action = self.table2_tapped
-    self.lst2.delete_enabled = False 
+    self.lst_local = ui.ListDataSource(all)
+    self.tv_local.data_source = self.lst_local
+    self.tv_local.delegate = self.lst_local
+    self.tv_local.editing = False
+    self.lst_local.font = ('Courier',14)
+    self.lst_local.action = self.table_local_tapped
+    self.lst_local.delete_enabled = False 
+    self.lst_remote = ui.ListDataSource([''])
+    self.tv_remote.data_source = self.lst_remote
+    self.tv_remote.delegate = self.lst_remote
+    self.tv_remote.editing = False
+    self.lst_remote.font = ('Courier',14)
+    self.lst_remote.action = self.table_remote_tapped
+    self.lst_remote.delete_enabled = False 
     self.view.present('fullscreen')
 
   def bt_connect(self, sender):
-    host = self.view['textfield1'].text
-    user = self.view['textfield2'].text
-    password = self.view['textfield3'].text
+    host = self.view['tf_host'].text
+    user = self.view['tf_user'].text
+    password = self.view['tf_password'].text
     port = 22
     if self.connect:
       self.connect = False 
       self.sftp.close()
       self.transport.close()
       sender.title = 'Connect'
-      self.txtv.text = ''
+      self.tv_info.text = ''
     else:
       self.connect = True
       self.transport = paramiko.Transport((host, port))
@@ -67,31 +64,32 @@ class SFTPclient(ui.View):
       all = sorted(remoteDir)
       for file in sorted(files):
         all.append('{}'.format(file))
-      self.refresh_table(self.tv2,self.lst2,all)
+      self.refresh_table(self.tv_remote,self.lst_remote,all)
+      self.view['lb_remote'].text = self.remotePath
       sender.title = 'Disconnect'
-      self.txtv.text = 'Connect with ' + host + ':' + str(port) + '\n'
-      #self.txtv.text += 'user: ' + user + ', password: ' + password + '\n'
-      #self.txtv.text += 'remotePath: ' + self.remotePath + '\n'
+      self.tv_info.text = 'Connect with ' + host + ':' + str(port) + '\n'
+      #self.tv_info.text = 'user: ' + user + ', password: ' + password + '\n' + self.tv_info.text
+      #self.tv_info.text = 'remotePath: ' + self.remotePath + '\n' + self.tv_info.text
 
   def bt_upload(self, sender):
     if self.connect:
       pos = self.localFile.rfind('/')
       fileName = self.localFile[pos+1:]
-      self.txtv.text += 'put(' + self.localFile + ', ' + self.remotePath + '/' + fileName + ')' + '\n'
+      self.tv_info.text = 'put(' + self.localFile + ', ' + self.remotePath + '/' + fileName + ')' + '\n' + self.tv_info.text
       self.sftp.put(self.localFile, self.remotePath + '/' + fileName)
       all = self.get_remote_dir()
-      self.refresh_table(self.tv2,self.lst2,all)
+      self.refresh_table(self.tv_remote,self.lst_remote,all)
 
   def bt_download(self, sender):
     if self.connect:
       pos = self.remoteFile.rfind('/')
       fileName = self.remoteFile[pos+1:]
-      self.txtv.text += 'get(' + self.remoteFile + ', ' + fileName + ')' + '\n'
+      self.tv_info.text = 'get(' + self.remoteFile + ', ' + fileName + ')' + '\n' + self.tv_info.text
       self.sftp.get(self.remoteFile, fileName)
       all = self.get_dir()
-      self.refresh_table(self.tv1,self.lst1,all)
+      self.refresh_table(self.tv_local,self.lst_local,all)
 
-  def table1_tapped(self, sender):
+  def table_local_tapped(self, sender):
     rowtext = sender.items[sender.selected_row]
     filename_tapped = rowtext
     if rowtext[0] == '/':
@@ -101,12 +99,14 @@ class SFTPclient(ui.View):
       else:
         self.path = self.path + filename_tapped
       all = self.get_dir()
-      self.view.name = self.path
-      self.refresh_table(self.view['tableview1'],self.lst1,all)
+      #self.view.name = self.path
+      root_len = len(self.root)
+      self.view['lb_local'].text = self.path[root_len:]
+      self.refresh_table(self.view['tv_local'],self.lst_local,all)
     else:
       self.localFile = self.path + '/' + filename_tapped
 
-  def table2_tapped(self, sender):
+  def table_remote_tapped(self, sender):
     rowtext = sender.items[sender.selected_row]
     filename_tapped = rowtext
     if rowtext[0] == '/':
@@ -121,8 +121,9 @@ class SFTPclient(ui.View):
         else:
           self.remotePath = self.remotePath + filename_tapped
       all = self.get_remote_dir()
-      self.refresh_table(self.tv2,self.lst2,all)
-      self.txtv.text = 'remotePath: ' + self.remotePath + '\n' + self.txtv.text
+      self.refresh_table(self.tv_remote,self.lst_remote,all)
+      #self.tv_info.text = 'remotePath: ' + self.remotePath + '\n' + self.tv_info.text
+      self.view['lb_remote'].text = self.remotePath
     else:
       self.remoteFile = self.remotePath + '/' + filename_tapped
 
@@ -132,10 +133,10 @@ class SFTPclient(ui.View):
     table.delegate = lst
     table.editing = False
     lst.font = ('Courier',14)
-    if table.name == 'tableview1':
-      lst.action = self.table1_tapped
+    if table.name == 'tv_local':
+      lst.action = self.table_local_tapped
     else:
-      lst.action = self.table2_tapped
+      lst.action = self.table_remote_tapped
     lst.delete_enabled = False 
     table.reload_data()
     return
